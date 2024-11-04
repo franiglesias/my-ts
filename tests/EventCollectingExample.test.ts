9f76a9eb84f991b84004af889cea0ad8fe846571
Fran Iglesias <fran.iglesias@qualifyze.com>
2024-11-04 14:30:12 +0100

    Separate concerns

--- tests/EventCollectingExample.test.ts
+++ tests/EventCollectingExample.test.ts
@@ -1,8 +1,8 @@
 import {test} from "node:test";
-import {SomeEntity} from "../src/SomeEntity";
-import {SomethingWasDone} from "../src/SomethingWasDone";
+import {SomeEntity} from "../src/event-collecting-example/SomeEntity";
+import {SomethingWasDone} from "../src/event-collecting-example/SomethingWasDone";
 import assert from "node:assert";
-import {DomainEvent} from "../src/DomainEvent";
+import {DomainEvent} from "../src/event-collecting-example/DomainEvent";
 
 
 test('Event collecting works', (): void => {
